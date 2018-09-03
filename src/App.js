455046ad032c28242700cb65466755cdafb1c378
Robert Pirritano <rpirritano@gmail.com>
2018-09-03 12:55:01 -0700

    created MainInterface component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import './wisdompetlogo.svg'
 
-class App extends Component {
+class MainInterface extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="interface">
+        <h1>Pet Appointments</h1>
+        <ul>
+          <li>Buffy 3:30 PM</li>
+          <li>Spot 8:30 PM</li>
+          <li>Goldie 3:50 PM</li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default MainInterface;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './wisdompetlogo.svg'
+import MainInterface from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainInterface />, document.getElementById('petAppointments'));
 registerServiceWorker();
