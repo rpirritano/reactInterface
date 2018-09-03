978dcb1d931fd8e65a9ca5806dcde1acf6c3bba3
Robert Pirritano <rpirritano@gmail.com>
2018-09-03 16:34:31 -0700

    started adding form component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import React, { Component } from 'react'
 import './App.css'
 import './wisdompetlogo.svg'
 import AptList from './AptList'
+import AddAppointment from './AddAppointment'
 
 class MainInterface extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      aptBodyVisible: true, //make false when fix code
       data: [
         {
+          "id": 0,
           "petName": "Buffy",
           "ownerName": "Hassum Harrod",
           "aptDate": "2016-06-20 15:30",
           "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
         },
         {
+          "id": 1,
           "petName": "Spot",
           "ownerName": "Constance Smith",
           "aptDate": "2016-06-24 08:30",
           "aptNotes": "This German Shepherd is having some back pain"
         },
         {
+          "id": 2,
           "petName": "Goldie",
           "ownerName": "Barot Bellingham",
           "aptDate": "2016-06-22 15:50",
           "aptNotes": "This Goldfish has some weird spots in the belly"
         },
         {
+          "id": 3,
           "petName": "Mitten",
           "ownerName": "Hillary Goldwyn",
           "aptDate": "2016-06-21 9:15",
@@ -34,19 +40,47 @@ class MainInterface extends Component {
         }
       ]
     }
+
   } //set inital state
+
+  deleteMessage(id) {
+    console.log('removing item at', id)
+
+  }
+//TO have form diplay/hide
+//  toggleAddDisplay: function() {
+//    let tempVisibility = !this.state.aptBodyVisible;
+//    this.setState({
+//      aptBodyVisible: tempVisibility
+//    });
+//  },
+
+//addItem: function(tempItem) {
+//  let tempApts = this.set.myAppointments;
+//  tempApts.push(tempItem);
+//  this.setState({
+//    myAppointments:tempApts
+//  });
+//},
 
   render() {
     let filteredApts = this.state.data;
     filteredApts = filteredApts.map(function(item, index) {
       return(
         <AptList key={ index }
-          singleItem = { item } />
+          singleItem = { item }
+          whichItem = { item }
+          onDelete = { this.deleteMessage } />
       )
     }.bind(this));
 
     return (
       <div className="interface">
+        <AddAppointment
+          bodyVisible = { this.state.aptBodyVisible }
+          handleToggle = { this.toggleAddDisplay }
+          addApt = { this.addItem }
+        />
         <ul className="item-list media-list">{filteredApts}</ul>
       </div>
     )
--- src/AptList.js
+++ src/AptList.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 
 
 class AptList extends Component {
+
+
+
   render(){
     return (
       <li className="pet-item media">
+        <div className="media-left">
+          <button className="pet-delete btn btn-xs btn-danger" onClick={this.handleDelete}>
+          <span className="glyphicon glyphicon-remove"></span></button>
+        </div>
         <div className="pet-info media-body">
           <div className="pet-head">
             <span className="pet-name">{this.props.singleItem.petName}</span>
