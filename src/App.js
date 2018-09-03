53ecb5d79e4e6f2c7322e60318901a361be4c0b4
Robert Pirritano <rpirritano@gmail.com>
2018-09-03 13:25:03 -0700

    added inital state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css'
 import './wisdompetlogo.svg'
 
 class MainInterface extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: 'Appointments', show: true}
+  } //set inital state
+
   render() {
+    let showTitle;
+    this.state.show ? showTitle = 'New' + ' ' : null
+
+    let displayList = {
+      display: this.state.show ? 'block':'none',
+      color: 'red'
+    }
+
+
     return (
       <div className="interface">
-        <h1>Pet Appointments</h1>
-        <ul>
+        <h1>{ showTitle }
+          { this.state.title }</h1>
+        <ul style={displayList}>
           <li>Buffy 3:30 PM</li>
           <li>Spot 8:30 PM</li>
           <li>Goldie 3:50 PM</li>
