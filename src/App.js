6a4adc89da0e346ec249b336f29747bb4d2fa1dc
Robert Pirritano <rpirritano@gmail.com>
2018-09-03 14:05:05 -0700

    hard-coded array in state

--- src/App.js
+++ src/App.js
@@ -5,31 +5,57 @@ import './wisdompetlogo.svg'
 class MainInterface extends Component {
   constructor(props) {
     super(props);
-    this.state = {title: 'Appointments', show: true}
+    this.state = {
+      data: [
+        {
+          "petName": "Buffy",
+          "ownerName": "Hassum Harrod",
+          "aptDate": "2016-06-20 15:30",
+          "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
+        },
+        {
+          "petName": "Spot",
+          "ownerName": "Constance Smith",
+          "aptDate": "2016-06-24 08:30",
+          "aptNotes": "This German Shepherd is having some back pain"
+        },
+        {
+          "petName": "Goldie",
+          "ownerName": "Barot Bellingham",
+          "aptDate": "2016-06-22 15:50",
+          "aptNotes": "This Goldfish has some weird spots in the belly"
+        },
+        {
+          "petName": "Mitten",
+          "ownerName": "Hillary Goldwyn",
+          "aptDate": "2016-06-21 9:15",
+          "aptNotes": "Cat has excessive hairballs"
+        }
+      ]
+    }
   } //set inital state
 
   render() {
-    let showTitle;
-    this.state.show ? showTitle = 'New' + ' ' : null
-
-    let displayList = {
-      display: this.state.show ? 'block':'none',
-      color: 'red'
-    }
-
 
     return (
       <div className="interface">
-        <h1>{ showTitle }
-          { this.state.title }</h1>
-        <ul style={displayList}>
-          <li>Buffy 3:30 PM</li>
-          <li>Spot 8:30 PM</li>
-          <li>Goldie 3:50 PM</li>
+        <ul className="item-list media-list">
+          <li className="pet-item media">
+            <div className="pet-info media-body">
+              <div className="pet-head">
+                <span className="pet-name">{this.state.data[0].petName}</span>
+                <span className="apt-date pull-right">{this.state.data[0].aptDate}</span>
+              </div>
+              <div className="owner-name"><span className="label-item">Owner:</span>
+                {this.state.data[0].ownerName}</div>
+              <div className="apt-notes">{this.state.data[0].aptNotes}</div>
+            </div>
+          </li>
         </ul>
       </div>
     )
   }
 }
+
 
 export default MainInterface;
